0bcbf3c4eddc46f4cca5c357a45186614bf53bb9
haleluya001 <haleluyatigabe1990@gmail.com>
2025-09-07 16:07:11 +0300

    Add django-models project with relationship_app, models, and query_samples.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,16 @@
+from . import views
 from django.urls import path
-from .views import list_books, LibraryDetailView, register_view
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    # Function-based registration view
+    path('register/', views.register, name='register'),
 
-    # Authentication URLs
-    path('register/', register_view, name='register'),
+    # Built-in authentication views
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+    # Your other views
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
