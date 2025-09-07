66c536a98aa79106595576233d86ea887a0db6c4
haleluya001 <haleluyatigabe1990@gmail.com>
2025-09-07 15:43:03 +0300

    Add django-models project with relationship_app, models, and query_samples.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from .models import Book, Library  # ✅ Make sure both Book and Library are imported
+from .models import Book, Library  # Make sure both Book and Library are imported
 
 # Function-based view: list all books
 def list_books(request):
