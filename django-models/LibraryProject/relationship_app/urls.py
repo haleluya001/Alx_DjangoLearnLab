f25c611ccfda86b3f3c23e63cd5b1c3f3549b51c
haleluya001 <haleluyatigabe1990@gmail.com>
2025-09-07 16:26:29 +0300

    Add django-models project with relationship_app, models, and query_samples.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('member-view/', views.member_view, name='member_view'),
 
      # Book management with permissions
-    path('books/add/', views.add_book, name='add_book'),
+    path('books/add/', views.add_book, name='add_book'),          
     path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
     path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
 
