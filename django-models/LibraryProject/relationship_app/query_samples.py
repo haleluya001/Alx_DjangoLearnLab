0da412aee385dedee99c0af8dd8ca1354aa1b6e5
haleluya001 <haleluyatigabe1990@gmail.com>
2025-09-07 15:15:01 +0300

    Add django-models project with relationship_app, models, and query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,27 +1,31 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-# 1. Query all books by a specific author
+# 1 Query all books by a specific author
 def get_books_by_author(author_name):
     try:
         author = Author.objects.get(name=author_name)
-        return author.books.all()
+        # Correct filter query
+        books = Book.objects.filter(author=author)
+        return books
     except Author.DoesNotExist:
         return []
 
 
-# 2. List all books in a library
+# 2 List all books in a library
 def get_books_in_library(library_name):
     try:
         library = Library.objects.get(name=library_name)
-        return library.books.all()
+        books = library.books.all()  # ManyToMany relation
+        return books
     except Library.DoesNotExist:
         return []
 
 
-# 3. Retrieve the librarian for a library
+# 3 Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
     try:
         library = Library.objects.get(name=library_name)
-        return library.librarian
+        librarian = library.librarian  # OneToOne relation
+        return librarian
     except (Library.DoesNotExist, Librarian.DoesNotExist):
         return None
