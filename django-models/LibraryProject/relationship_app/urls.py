c9d70a00feb016ff2ae3739cd96123662f77b333
haleluya001 <haleluyatigabe1990@gmail.com>
2025-09-07 15:39:49 +0300

    Add django-models project with relationship_app, models, and query_samples.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView  # ✅ Import views here
+from .views import list_books, LibraryDetailView  # Import views here
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
