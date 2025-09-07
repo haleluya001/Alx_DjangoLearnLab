57bdd7007b032a04025bddeb58d9bf6ad9d13fbc
haleluya001 <haleluyatigabe1990@gmail.com>
2025-09-07 15:37:42 +0300

    Add django-models project with relationship_app, models, and query_samples.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-from .models import Book, Library
-from django.views.generic import DetailView
+from .models import Book
+from django.views.generic.detail import DetailView
+from .models import Library
 
+# Function-based view
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
+# Class-based view
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
