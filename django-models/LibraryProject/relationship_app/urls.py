a011a81f8964f0c2f016b84f2f493850632392e2
haleluya001 <haleluyatigabe1990@gmail.com>
2025-09-07 16:04:55 +0300

    Add django-models project with relationship_app, models, and query_samples.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView  # ✅ Import both views
+from .views import (
+    list_books, LibraryDetailView,
+    register_view, login_view, logout_view
+)
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),  # Function-based view
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
