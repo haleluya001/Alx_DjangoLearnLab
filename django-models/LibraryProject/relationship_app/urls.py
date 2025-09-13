ff7924c4e5b96966ae49259eb1b6a90c2930cfa2
Haleluya Tigabe <haleluyatigabe1990@gmail.com>
2025-09-13 18:53:15 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -16,8 +16,8 @@ urlpatterns = [
 
     # Authentication
     path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout')
 
     # Role-based access
     path('admin-view/', views.admin_view, name='admin_view'),
