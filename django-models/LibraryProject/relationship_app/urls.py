d1735dca47c83fefd2d31c584511d717d3fefc85
haleluya001 <haleluyatigabe1990@gmail.com>
2025-09-21 01:08:09 +0300

    week 10

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,4 +6,9 @@ from .views import list_books
 urlpatterns = [
     path("books/", views.list_books, name="list_books"),  # FBV
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # CBV
+
+    # Authentication
+    path("register/", views.register_view, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout")
 ]
